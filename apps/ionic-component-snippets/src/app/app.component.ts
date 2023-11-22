f37a54397f03b9e88ca6f422aff1f05f1bee2f23
Lennon Reid <lenonrade@163.com>
2023-11-22 21:56:28 +0800

    build(deps): upgrade @ionic/angular to version 7.5.6 to address the reported bug in issue #28385 in ionic-framework

--- apps/ionic-component-snippets/src/app/app.component.ts
+++ apps/ionic-component-snippets/src/app/app.component.ts
@@ -1,34 +1,37 @@
 import { HttpClientModule } from '@angular/common/http';
-import { CUSTOM_ELEMENTS_SCHEMA, Component, EnvironmentInjector, OnInit, inject } from '@angular/core';
-import { IonApp, IonButton, IonContent, IonRouterOutlet, Platform } from '@ionic/angular/standalone';
+import {
+  CUSTOM_ELEMENTS_SCHEMA,
+  Component,
+  EnvironmentInjector,
+  OnInit,
+  inject,
+} from '@angular/core';
+import {
+  IonApp,
+  IonButton,
+  IonContent,
+  IonRouterOutlet,
+  Platform,
+} from '@ionic/angular/standalone';
 import { Capacitor } from '@capacitor/core';
 import { App } from '@capacitor/app';
 import { register } from 'swiper/element/bundle';
-import { defineCustomElement as defineLoading } from '@ionic/core/components/ion-loading';
-import { defineCustomElement as defineAlert } from '@ionic/core/components/ion-alert';
 
 import { addIcons } from 'ionicons';
-import {
-  chevronDown,
-  chevronUp
-} from 'ionicons/icons';
+import { chevronDown, chevronUp } from 'ionicons/icons';
 
 register();
 @Component({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  imports: [
-    HttpClientModule,
-    IonApp,
-    IonContent,
-    IonRouterOutlet,
-    IonButton
-  ],
+  imports: [HttpClientModule, IonApp, IonContent, IonRouterOutlet, IonButton],
   providers: [],
   selector: 'ionic-component-snippets-root',
   standalone: true,
   template: `
     <ion-app>
-        <ion-router-outlet [environmentInjector]="environmentInjector"></ion-router-outlet>
+      <ion-router-outlet
+        [environmentInjector]="environmentInjector"
+      ></ion-router-outlet>
     </ion-app>
   `,
 })
@@ -39,13 +42,9 @@ export class AppComponent implements OnInit {
     'chevron-down': chevronDown,
     'chevron-up': chevronUp,
   };
-  constructor(
-    private platform: Platform,
-  ) {
-    defineLoading();
-    defineAlert();
+  constructor(private platform: Platform) {
     this.initializeApp();
-    addIcons(this.iconMaps)
+    addIcons(this.iconMaps);
   }
   ngOnInit(): void {
     if (Capacitor.getPlatform() === 'android') {
@@ -67,5 +66,4 @@ export class AppComponent implements OnInit {
       else App.exitApp();
     });
   }
-
 }
