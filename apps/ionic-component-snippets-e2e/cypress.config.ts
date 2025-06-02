476ff3214917db4c8bc3dc7071194d6dd7a36187
Lennon Reid <lenonrade@163.com>
2025-06-02 22:51:24 +0800

    chore: [nx migration] set-inject-document-domain

--- apps/ionic-component-snippets-e2e/cypress.config.ts
+++ apps/ionic-component-snippets-e2e/cypress.config.ts
@@ -7,5 +7,8 @@ export default defineConfig({
     // Please ensure you use `cy.origin()` when navigating between domains and remove this option.
     // See https://docs.cypress.io/app/references/migration-guide#Changes-to-cyorigin
     injectDocumentDomain: true,
+    // Please ensure you use `cy.origin()` when navigating between domains and remove this option.
+    // See https://docs.cypress.io/app/references/migration-guide#Changes-to-cyorigin
+    injectDocumentDomain: true,
   },
 });
